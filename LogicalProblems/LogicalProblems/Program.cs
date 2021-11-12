22484f97f9495d05254c759b6c75e153bf7d039e
“VenuReddy773” <venu706@gmail.com>
2021-11-12 13:34:19 +0530

    [VenuGopalReddy] Added: Reverse Of Number Program

--- LogicalProblems/LogicalProblems/Program.cs
+++ LogicalProblems/LogicalProblems/Program.cs
@@ -9,7 +9,7 @@ namespace LogicalProblems
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Choose an Option To Execute the Program\n 1.FibonacciSeries\n 2.Exit");
+                Console.WriteLine("Choose an Option To Execute the Program\n 1.FibonacciSeries\n 2.ReverseOfNumber\n 3.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -20,6 +20,12 @@ namespace LogicalProblems
                         result.Fibonacci(number);
                         break;
                     case 2:
+                        Console.WriteLine("Enter The Number");
+                        int number1 = Convert.ToInt32(Console.ReadLine());
+                        ReverseOfaNumber result1 = new ReverseOfaNumber();
+                        result1.Reverse(number1);
+                        break;
+                    case 3:
                         flag = false;
                         break;
                     default:
---
+++ LogicalProblems/LogicalProblems/ReverseOfaNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProblems
+{
+    class ReverseOfaNumber
+    {
+        int reverseNumber = 0;
+        public void Reverse(int number)
+        {
+            while(number > 0)
+            {
+                int remainder = number % 10;
+                reverseNumber = reverseNumber * 10 + remainder;
+                number = number / 10;
+            }
+            Console.WriteLine("Reverse Of Entered Number is: "+reverseNumber+"\n");
+        }
+    }
+}
