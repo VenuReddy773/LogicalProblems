85007a4079159bc745e949f80334d37e9cf3ef1e
“VenuReddy773” <venu706@gmail.com>
2021-11-12 12:44:28 +0530

    [VenuGopalReddy] Added: Fibonacci Series Program

---
+++ LogicalProblems/LogicalProblems/FibonacciSeries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicalProblems
+{
+    class FibonacciSeries
+    {
+        int first = 0, second = 1, third = 0;
+        public void Fibonacci(int number)
+        {
+            if(number == 0)
+            {
+                Console.WriteLine(first);
+                return;
+            }
+            Console.WriteLine(first);
+            Console.WriteLine(second);
+            for(int i = 2; i < number; i++)
+            {
+                third = first + second;
+                first = second;
+                second = third;
+                Console.WriteLine( third);
+            }
+        }
+    }
+}
---
+++ LogicalProblems/LogicalProblems/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LogicalProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Choose an Option To Execute the Program\n 1.FibonacciSeries\n 2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter The Number");
+                        int number = Convert.ToInt32(Console.ReadLine());
+                        FibonacciSeries result = new FibonacciSeries();
+                        result.Fibonacci(number);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter The Correct Option..\n");
+                        break;
+                }
+            }
+        }
+    }
+}
